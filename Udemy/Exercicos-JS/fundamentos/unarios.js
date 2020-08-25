9a74dd231cb611937c7d35a0366f5cc30bff329f
Cleiton Franca <cleitonfranca05@gmail.com>
2020-08-24 21:11:39 -0300

    Aula de Erros,aritimeticos,ternaros,unarios

---
+++ Udemy/Exercicos-JS/fundamentos/unarios.js
@@ -0,0 +1,11 @@
+let num1 = 1 
+let num2 = 2
+
+num1++
+console.log (num1)
+
+--num1
+console.log(num1)
+
+console.log(++num1 === num2--) // Verdadeiro pq a comparaçãp vem antes de decrementar o num2
+console.log(num1 === num2)
